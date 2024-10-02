532b8cc473765bcb16dbf7f07ea1297b8006a3a5
Nathaniel Sullivan <nathaniel.sullivan@geminidsystems.com>
2024-10-01 23:21:14 -0700

    VARNS-40: Add delete button to edit user story page
    Modified the UserStoryWidget class to accept an action listener from the UserStoryListPane. This allows us to refresh the UserStoryListPane when the edit menu is closed.
    
    Also did some refactoring for the touched classes including creating seperate handler methods for the listeners to improve readability and use Single Responsibility Principle

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/UserStoryListPane.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/UserStoryListPane.java
@@ -1,21 +1,23 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels;
 
-import com.groupesan.project.java.scrumsimulator.mainpackage.impl.UserStory;
-import com.groupesan.project.java.scrumsimulator.mainpackage.impl.UserStoryStore;
-import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.BaseComponent;
-import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.UserStoryWidget;
-import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+
+import com.groupesan.project.java.scrumsimulator.mainpackage.impl.UserStory;
+import com.groupesan.project.java.scrumsimulator.mainpackage.impl.UserStoryStore;
+import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.BaseComponent;
+import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.UserStoryWidget;
+import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
 
 public class UserStoryListPane extends JFrame implements BaseComponent {
     public UserStoryListPane() {
@@ -23,31 +25,24 @@ public class UserStoryListPane extends JFrame implements BaseComponent {
     }
 
     private List<UserStoryWidget> widgets = new ArrayList<>();
+    private JPanel subPanel = new JPanel();
 
-    public void init() {
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setTitle("User Story list");
-        setSize(400, 300);
-
-        GridBagLayout myGridbagLayout = new GridBagLayout();
-        JPanel myJpanel = new JPanel();
-        myJpanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        myJpanel.setLayout(myGridbagLayout);
-
-        // demo/testing widgets
-        //        UserStory aUserStory = UserStoryFactory.getInstance().createNewUserStory("foo",
-        // "bar", 2);
-        //        UserStory aUserStory2 =
-        //                UserStoryFactory.getInstance().createNewUserStory("foo2", "bar2", 4);
-        //        widgets.add(new UserStoryWidget(aUserStory));
-        //        widgets.add(new UserStoryWidget(aUserStory2));
+    private void reloadUserStories() {
+        widgets.clear();
+        subPanel.removeAll();
 
         for (UserStory userStory : UserStoryStore.getInstance().getUserStories()) {
-            widgets.add(new UserStoryWidget(userStory));
+            UserStoryWidget userStoryWidget = new UserStoryWidget(userStory).setCloseEditDialogActionListener(
+                    new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            reloadUserStories();
+                        }
+                    });
+            userStoryWidget.init();
+            widgets.add(userStoryWidget);
         }
 
-        JPanel subPanel = new JPanel();
-        subPanel.setLayout(new GridBagLayout());
         int i = 0;
         for (UserStoryWidget widget : widgets) {
             subPanel.add(
@@ -61,42 +56,61 @@ public class UserStoryListPane extends JFrame implements BaseComponent {
                             GridBagConstraints.HORIZONTAL));
         }
 
+        subPanel.revalidate();
+        subPanel.repaint();
+    }
+
+    private ActionListener handleNewUserStoryAction() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NewUserStoryForm form = new NewUserStoryForm();
+                form.setVisible(true);
+
+                form.addWindowListener(
+                        new java.awt.event.WindowAdapter() {
+                            public void windowClosed(
+                                    java.awt.event.WindowEvent windowEvent) {
+                                UserStory userStory = form.getUserStoryObject();
+                                UserStoryStore.getInstance().addUserStory(userStory);
+                                reloadUserStories();
+                            }
+                        });
+            }
+        };
+    }
+
+    public void init() {
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("User Story list");
+        setSize(400, 300);
+
+        JPanel myJpanel = new JPanel();
+        myJpanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        myJpanel.setLayout(new GridBagLayout());
+
+        // demo/testing widgets
+        // UserStory aUserStory =
+        // UserStoryFactory.getInstance().createNewUserStory("foo",
+        // "bar", 2);
+        // UserStory aUserStory2 =
+        // UserStoryFactory.getInstance().createNewUserStory("foo2", "bar2", 4);
+        // widgets.add(new UserStoryWidget(aUserStory));
+        // widgets.add(new UserStoryWidget(aUserStory2));
+
+        subPanel.setLayout(new GridBagLayout());
+
+        reloadUserStories();
+
         myJpanel.add(
                 new JScrollPane(subPanel),
                 new CustomConstraints(
                         0, 0, GridBagConstraints.WEST, 1.0, 0.8, GridBagConstraints.HORIZONTAL));
 
-        JButton newSprintButton = new JButton("New User Story");
-        newSprintButton.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        NewUserStoryForm form = new NewUserStoryForm();
-                        form.setVisible(true);
-
-                        form.addWindowListener(
-                                new java.awt.event.WindowAdapter() {
-                                    public void windowClosed(
-                                            java.awt.event.WindowEvent windowEvent) {
-                                        UserStory userStory = form.getUserStoryObject();
-                                        UserStoryStore.getInstance().addUserStory(userStory);
-                                        widgets.add(new UserStoryWidget(userStory));
-                                        int idx = widgets.size() - 1;
-                                        subPanel.add(
-                                                widgets.get(idx),
-                                                new CustomConstraints(
-                                                        0,
-                                                        idx,
-                                                        GridBagConstraints.WEST,
-                                                        1.0,
-                                                        0.1,
-                                                        GridBagConstraints.HORIZONTAL));
-                                    }
-                                });
-                    }
-                });
+        JButton newUserStory = new JButton("New User Story");
+        newUserStory.addActionListener(handleNewUserStoryAction());
         myJpanel.add(
-                newSprintButton,
+                newUserStory,
                 new CustomConstraints(
                         0, 1, GridBagConstraints.WEST, 1.0, 0.2, GridBagConstraints.HORIZONTAL));
 
