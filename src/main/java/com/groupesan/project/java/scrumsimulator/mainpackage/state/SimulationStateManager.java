a3a65647a4d072d29683925873da02e2e3589303
srchowd3-445 <suparnoroy2001@gmail.com>
2024-10-07 14:36:32 -0700

    Trying to debug simulation.JSON error

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/state/SimulationStateManager.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/state/SimulationStateManager.java
@@ -158,6 +158,7 @@ public class SimulationStateManager {
                         sprint++;
                     }
                     //runSimulation();
+                    runSimulation();
 
                 }
             }
