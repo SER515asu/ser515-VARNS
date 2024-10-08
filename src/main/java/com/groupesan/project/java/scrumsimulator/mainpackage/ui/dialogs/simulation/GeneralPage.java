3dcde1d54c2924fad61469235a1dbbd309b30ce4
vkamani1 <vkamani1@asu.edu>
2024-10-07 22:08:50 -0700

    VARNS-46: Added Range Selection UI, for Sprint length and sprint number, along with submit button and Message window.

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/dialogs/simulation/GeneralPage.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/dialogs/simulation/GeneralPage.java
@@ -6,77 +6,100 @@ import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.Resuable
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.SpinnerInput;
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.TextInput;
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.Wizard;
+import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
+import com.groupesan.project.java.scrumsimulator.mainpackage.ui.dialogs.simulation.AutoFillToggleButton;
+
 import java.awt.*;
 import javax.swing.*;
 
 class GeneralPage extends Wizard.WizardPage {
-    private final DataModel<String> simulationModel;
-    private final DataModel<Object> sprintModel;
-    private final DataModel<Object> sprintLengthModel;
+        private final DataModel<String> simulationModel;
+        private final DataModel<Object> sprintModel;
+        private final DataModel<Object> sprintLengthModel;
 
-    public GeneralPage(DataModel<String> simulationModel, DataModel<Object> sprintModel, DataModel<Object> sprintLengthModel) {
-        this.simulationModel = simulationModel;
-        this.sprintModel = sprintModel;
-        this.sprintLengthModel = sprintLengthModel;
-    }
+        public GeneralPage(DataModel<String> simulationModel, DataModel<Object> sprintModel,
+                        DataModel<Object> sprintLengthModel) {
+                this.simulationModel = simulationModel;
+                this.sprintModel = sprintModel;
+                this.sprintLengthModel = sprintLengthModel;
+        }
 
-    @Override
-    protected String getId() {
-        return "General";
-    }
+        @Override
+        protected String getId() {
+                return "General";
+        }
 
-    @Override
-    protected JPanel render() {
-        JPanel container = new JPanel(new BorderLayout());
-        ResuableHeader resuableHeader =
-                new ResuableHeader("General", "General simulation settings");
+        @Override
+        protected JPanel render() {
+                JPanel container = new JPanel(new BorderLayout());
+                ResuableHeader resuableHeader = new ResuableHeader("General", "General simulation settings");
 
-        JPanel inputs = new JPanel(new GridBagLayout());
-        TextInput simulationInput =
-                new TextInput(
-                        "Name: ", new JTextField(simulationModel.getData(), 5), simulationModel);
-        SpinnerInput sprintInput =
-                new SpinnerInput(
-                        "Sprints: ",
-                        new JSpinner(new SpinnerNumberModel(1, 1, 20, 1)),
-                        sprintModel);
-        SpinnerInput sprintLengthInput =
-        new SpinnerInput(
-                "Sprint Length (days): ",
-                new JSpinner(new SpinnerNumberModel(14, 1, 30, 1)),
-                sprintLengthModel);
+                JPanel inputs = new JPanel(new GridBagLayout());
+                TextInput simulationInput = new TextInput(
+                                "Name: ", new JTextField(simulationModel.getData(), 5), simulationModel);
+                SpinnerInput sprintInput = new SpinnerInput(
+                                "Sprints: ",
+                                new JSpinner(new SpinnerNumberModel(1, 1, 20, 1)),
+                                sprintModel);
+                SpinnerInput sprintLengthInput = new SpinnerInput(
+                                "Sprint Length (days): ",
+                                new JSpinner(new SpinnerNumberModel(14, 1, 30, 1)),
+                                sprintLengthModel);
 
-        inputs.add(
-                resuableHeader,
-                new GridBagConstraintsBuilder()
-                        .setGridX(0)
-                        .setGridY(0)
-                        .setWeightX(1)
-                        .setFill(GridBagConstraints.HORIZONTAL)
-                        .setInsets(new Insets(0, 0, 5, 0)));
-        inputs.add(
-                simulationInput,
-                new GridBagConstraintsBuilder()
-                        .setGridX(0)
-                        .setGridY(1)
-                        .setWeightX(1)
-                        .setFill(GridBagConstraints.HORIZONTAL));
-        inputs.add(
-                sprintInput,
-                new GridBagConstraintsBuilder()
-                        .setGridX(0)
-                        .setGridY(2)
-                        .setWeightX(1)
-                        .setFill(GridBagConstraints.HORIZONTAL));
-        inputs.add(
-        sprintLengthInput,
-        new GridBagConstraintsBuilder()
-                .setGridX(0)
-                .setGridY(3)
-                .setWeightX(1)
-                .setFill(GridBagConstraints.HORIZONTAL));
+                inputs.add(
+                                resuableHeader,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(0)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL)
+                                                .setInsets(new Insets(0, 0, 5, 0)));
+                inputs.add(
+                                simulationInput,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(1)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL));
+                inputs.add(
+                                sprintInput,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(2)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL));
+                inputs.add(
+                                sprintLengthInput,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(3)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL));
 
-        container.add(inputs, BorderLayout.NORTH);
-        return container;
-    }
+                // // Toggle Button for manual or auto fill the range of sprint length.
+                // JLabel autoFillLabel = new JLabel("Auto Fill:");
+                // JToggleButton toggleButton = new JToggleButton("OFF");
+
+                // toggleButton.addActionListener(e -> {
+                // // we will Call the random range function here later on
+                // if (toggleButton.isSelected()) {
+                // toggleButton.setText("ON");
+                // } else {
+                // toggleButton.setText("OFF");
+                // }
+                // });
+
+                // Use the AutoFillToggleButton class
+                AutoFillToggleButton autoFillToggleButton = new AutoFillToggleButton();
+                JPanel togglePanel = autoFillToggleButton.getPanel();
+
+                inputs.add(togglePanel, new GridBagConstraintsBuilder()
+                                .setGridX(2)
+                                .setGridY(4)
+                                .setWeightX(1)
+                                .setFill(GridBagConstraints.HORIZONTAL));
+
+                container.add(inputs, BorderLayout.NORTH);
+                return container;
+        }
 }
