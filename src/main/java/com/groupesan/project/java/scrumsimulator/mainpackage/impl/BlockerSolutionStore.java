09c778e00850227c5a2343fb0986b98bc6512e27
Nathaniel Sullivan <nathaniel.sullivan@geminidsystems.com>
2024-10-25 21:44:02 -0700

    [VARNS-90] Adding the BlockerSolution data model

---
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/impl/BlockerSolutionStore.java
@@ -0,0 +1,33 @@
+package com.groupesan.project.java.scrumsimulator.mainpackage.impl;
+
+import java.util.List;
+
+import com.groupesan.project.java.scrumsimulator.mainpackage.core.BlockerSolution;
+import com.groupesan.project.java.scrumsimulator.mainpackage.utils.RandomUtils;
+
+public class BlockerSolutionStore {
+
+    private static final List<BlockerSolution> blockerSolutions = List.of(
+            new BlockerSolution("Solution 1", 100),
+            new BlockerSolution("Solution 2", 100),
+            new BlockerSolution("Solution 3", 100),
+            new BlockerSolution("Solution 4", 100),
+            new BlockerSolution("Solution 5", 100));
+
+    private BlockerSolutionStore() {
+    }
+
+    public static BlockerSolution getRandomBlockerSolution() {
+        int totalWeight = blockerSolutions.stream().mapToInt(BlockerSolution::getChance).sum();
+        int randomValue = RandomUtils.getRandomInt(totalWeight);
+
+        int cumulativeWeight = 0;
+        for (BlockerSolution solution : blockerSolutions) {
+            cumulativeWeight += solution.getChance();
+            if (randomValue < cumulativeWeight) {
+                return solution;
+            }
+        }
+        return null;
+    }
+}
