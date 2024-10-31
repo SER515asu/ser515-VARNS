4865bd42f0f34993d10605d4b2a37a07a6a6d95c
atran50 <atran50@asu.edu>
2024-10-31 06:17:07 -0700

    [VARNS-93] Add saving simulations if user closes via exit code

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ScrumSimulator.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ScrumSimulator.java
@@ -1,9 +1,13 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage;
 
+import com.groupesan.project.java.scrumsimulator.mainpackage.state.SimulationSingleton;
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.App;
 
 public class ScrumSimulator {
     public static void main(String[] args) {
         new App().start();
+
+        Runtime.getRuntime()
+                .addShutdownHook(new Thread(() -> SimulationSingleton.getInstance().saveSimulationDetails()));
     }
 }
